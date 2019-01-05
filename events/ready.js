905c6a4b3643637d2c862cbea9bc0bd458c4b83a
alidirza <38112259+alidirza@users.noreply.github.com>
2019-01-05 11:13:17 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -21,6 +21,6 @@ module.exports = client => {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setGame(oyun[random], "https://www.twitch.tv/emirhansaracyt");
+        client.user.setGame(oyun[random], "https://www.twitch.tv/gamer_ailesi");
         }, 2 * 2500);
 }
