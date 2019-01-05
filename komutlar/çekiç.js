b94dc61202f84cd227c378098af22c58c8103352
alidirza <38112259+alidirza@users.noreply.github.com>
2019-01-05 12:12:01 +0300

    Create çekiç.js

---
+++ komutlar/çekiç.js
@@ -0,0 +1,26 @@
+hash komutlar/çekiç.js atınız.
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+    let mesaj = args.slice(0).join(' ');
+    if (mesaj.length < 1) return message.reply('**Kime Çekiç Atcağımı Yazmalısın**');
+    const embed = new Discord.RichEmbed()
+    .setAuthor('')
+  .setColor("RANDOM")
+    .setDescription(`** ${mesaj} ` + message.author.username + ' Sana 🔨 Attı. Canın Acımış Olmalı!**')
+    return message.channel.sendEmbed(embed);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+ kategori: "eğlence"
+};
+
+exports.help = {
+  name: 'çekiç',
+  description: 'İstediğiniz Kişiye Çekiç Atarsınız.',
+  usage: 'çekiç'
+};
