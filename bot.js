da93bbeefcf25d5c6625e719c3be5813f4686fda
alidirza <38112259+alidirza@users.noreply.github.com>
2019-01-05 12:18:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -280,3 +280,15 @@ client.on("message", async message => {
   if(commandfile) commandfile.run(client, message, args)
 
 })
+
+client.on("message", message => {
+    const dmchannel = client.channels.find("name", "dm-log");
+    if (message.channel.type === "dm") {
+        if (message.author.bot) return;
+        dmchannel.sendMessage("", {embed: {
+            color: 3447003,
+            title: `Gönderen: ${message.author.tag}`,
+            description: `Bota Özelden Gönderilen DM: ${message.content}`
+        }})
+    }
+});
