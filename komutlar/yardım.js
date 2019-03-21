b2c1c263910633b7011013203390ef983341619d
alidirza <38112259+alidirza@users.noreply.github.com>
2019-03-21 22:04:17 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,40 +1,31 @@
 const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
 
-var prefix = ayarlar.prefix;
 
-exports.run = (client, message, params) => {
-
-  if (!params[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
-  if (message.channel.type !== 'dm') {
-    const ozelmesajkontrol = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Özel mesajlarını kontrol et. :postbox:');
-    message.channel.sendEmbed(ozelmesajkontrol) }
-  } else {
-    let command = params[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
-    }
-  }
-
+exports.run = function(client, message) {
+const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('» Komut Grupları')
+.setTimestamp()
+.addField('» Eğlence Komutları', 'f?eğlence')
+.addField('» Moderatör Komutları', 'f?moderatör')
+.addField('» Genel Komutlar', 'f?genel')
+.addField('» Ekstra Komutlar', 'f?ekstra')
+.addField('» Nsfw Komutları', 'f?nsfw')
+.setFooter('© 2018 Fredux', client.user.avatarURL)
+.setTimestamp()
+.setThumbnail(client.user.avatarURL)
+message.channel.send(embed)
 };
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ['h', 'halp', 'help', 'y'],
-  permLevel: 0
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0 
 };
 
 exports.help = {
   name: 'yardım',
   description: 'Tüm komutları gösterir.',
-  usage: 'yardım [komut]'
+  usage: 'yardım'
 };
